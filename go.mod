a74488494736ca9e498e39fd0bae8efa04c589a5
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2023-12-19 11:59:14 +0900

    rewrite

--- go.mod
+++ go.mod
@@ -1,26 +1,43 @@
 module github.com/mattn/nostr-forwarder
 
-go 1.20
+go 1.21.0
+
+toolchain go1.21.1
 
 require (
-	github.com/fiatjaf/relayer/v2 v2.0.0-20230508154911-c4a678da1ec9
-	github.com/nbd-wtf/go-nostr v0.18.0
-	go.uber.org/atomic v1.11.0
+	github.com/fiatjaf/eventstore v0.3.1
+	github.com/fiatjaf/relayer/v2 v2.1.9
+	github.com/nbd-wtf/go-nostr v0.27.2
 )
 
 require (
-	github.com/SaveTheRbtz/generic-sync-map-go v0.0.0-20230201052002-6c5833b989be // indirect
+	github.com/andybalholm/brotli v1.0.6 // indirect
 	github.com/btcsuite/btcd/btcec/v2 v2.3.2 // indirect
-	github.com/btcsuite/btcd/chaincfg/chainhash v1.0.2 // indirect
+	github.com/btcsuite/btcd/chaincfg/chainhash v1.1.0 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/decred/dcrd/crypto/blake256 v1.0.1 // indirect
 	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.2.0 // indirect
+	github.com/dgraph-io/ristretto v0.1.1 // indirect
+	github.com/dustin/go-humanize v1.0.1 // indirect
+	github.com/fasthttp/websocket v1.5.7 // indirect
 	github.com/gobwas/httphead v0.1.0 // indirect
 	github.com/gobwas/pool v0.2.1 // indirect
-	github.com/gobwas/ws v1.2.0 // indirect
-	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/gobwas/ws v1.3.1 // indirect
+	github.com/golang/glog v1.2.0 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
+	github.com/klauspost/compress v1.17.4 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
-	github.com/rs/cors v1.7.0 // indirect
-	golang.org/x/exp v0.0.0-20230425010034-47ecfdc1ba53 // indirect
-	golang.org/x/sys v0.8.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/puzpuzpuz/xsync/v2 v2.5.1 // indirect
+	github.com/rs/cors v1.10.1 // indirect
+	github.com/savsgio/gotils v0.0.0-20230208104028-c358bd845dee // indirect
+	github.com/tidwall/gjson v1.17.0 // indirect
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/pretty v1.2.1 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.51.0 // indirect
+	golang.org/x/exp v0.0.0-20231214170342-aacd6d4b4611 // indirect
+	golang.org/x/net v0.19.0 // indirect
+	golang.org/x/sys v0.15.0 // indirect
+	golang.org/x/time v0.5.0 // indirect
 )
--- main.go
+++ main.go
@@ -4,18 +4,11 @@ import (
 	"context"
 	"errors"
 	"flag"
-	"fmt"
-	"io"
 	"log"
-	"net/http"
-	"net/url"
-	"regexp"
-	"sort"
-	"strings"
 	"sync"
 	"sync/atomic"
-	"time"
 
+	"github.com/fiatjaf/eventstore"
 	"github.com/fiatjaf/relayer/v2"
 	"github.com/nbd-wtf/go-nostr"
 )
@@ -24,100 +17,64 @@ const (
 	urlPattern = `https?://[-A-Za-z0-9+&@#\/%?=~_|!:,.;\(\)]+`
 )
 
-type Relay struct {
-	URL   string
-	Write bool
+type forwarder struct {
+	ctx    context.Context
+	relays []string
+	pool   *nostr.SimplePool
 }
 
 var (
-	rewriteURLs bool
-	urlRe       = regexp.MustCompile(urlPattern)
+	_ relayer.Relay = (*forwarder)(nil)
 )
 
-var relays = []Relay{
-	{URL: "wss://nostr.compile-error.net/", Write: true},
-	{URL: "wss://relay.nostr.band/", Write: true},
-	{URL: "wss://nostr-relay.nokotaro.com/", Write: true},
-	{URL: "wss://universe.nostrich.land/?lang=ja&lang=en", Write: false},
-	{URL: "wss://relay.nostrich.land/", Write: true},
-	{URL: "wss://relay-jp.nostr.wirednet.jp/", Write: true},
-	{URL: "wss://nostr.wine/", Write: true},
-	{URL: "wss://nostr.h3z.jp/", Write: true},
+func (d *forwarder) Name() string {
+	return "ForwardOnlyRelay"
 }
 
-type forwarder struct {
-	relays []*nostr.Relay
-}
-
-func (d *forwarder) Name() string { return "ForwardOnlyRelay" }
 func (d *forwarder) Init() error {
-	if len(d.relays) > 0 {
-		return nil
-	}
-	d.relays = make([]*nostr.Relay, len(relays))
-	go func() {
-		for i := range relays {
-			if d.relays[i] == nil {
-				rr, err := nostr.RelayConnect(context.TODO(), relays[i].URL)
-				if err != nil {
-					continue
-				}
-				d.relays[i] = rr
-				log.Println("connected", relays[i])
-			} else if d.relays[i].ConnectionError != nil {
-				log.Println("closed", relays[i])
-				d.relays[i] = nil
-				continue
-			}
+	d.ctx = context.Background()
+	d.pool = nostr.NewSimplePool(d.ctx)
+	for _, v := range d.relays {
+		_, err := d.pool.EnsureRelay(v)
+		if err != nil {
+			log.Println(err)
 		}
-		time.Sleep(10 * time.Second)
-	}()
+	}
 	return nil
 }
-func (f *forwarder) Storage(ctx context.Context) relayer.Storage {
+
+func (f *forwarder) Storage(ctx context.Context) eventstore.Store {
 	return f
 }
 
-func (d *forwarder) BeforeSave(ctx context.Context, evt *nostr.Event) {
+func (f *forwarder) Close() {
 }
 
-func (d *forwarder) AfterSave(ctx context.Context, evt *nostr.Event) {
+func (d *forwarder) DeleteEvent(ctx context.Context, ev *nostr.Event) error {
+	return nil
 }
 
 func (d *forwarder) AcceptEvent(ctx context.Context, evt *nostr.Event) bool {
 	return true
 }
 
-func (d *forwarder) DeleteEvent(ctx context.Context, id string, pubkey string) error {
-	log.Println("DeleteEvent", id)
-
-	fmt.Println(id, pubkey)
-	return nil
-}
-
 func (d *forwarder) SaveEvent(ctx context.Context, event *nostr.Event) error {
-	log.Println("SaveEvent", event.Kind)
-
 	var success atomic.Int64
 	var wg sync.WaitGroup
-	for i, r := range d.relays {
-		if r == nil || !relays[i].Write {
-			continue
-		}
+	d.pool.Relays.Range(func(k string, v *nostr.Relay) bool {
 		wg.Add(1)
-		r := r
-		go func() {
+		go func(v *nostr.Relay) {
 			defer wg.Done()
-			status, err := r.Publish(context.Background(), *event)
+			err := v.Publish(context.Background(), *event)
 			if err != nil {
-				log.Println("error", r.URL, err)
+				log.Println("error", v.URL, err)
 				return
 			}
-			if err == nil && status != nostr.PublishStatusFailed {
-				success.Add(1)
-			}
-		}()
-	}
+			success.Add(1)
+		}(v)
+		return true
+	})
+
 	wg.Wait()
 	if success.Load() == 0 {
 		return errors.New("cannot post")
@@ -125,100 +82,41 @@ func (d *forwarder) SaveEvent(ctx context.Context, event *nostr.Event) error {
 	return nil
 }
 
-func (d *forwarder) QueryEvents(ctx context.Context, filter *nostr.Filter) (chan *nostr.Event, error) {
+func (d *forwarder) QueryEvents(ctx context.Context, filter nostr.Filter) (chan *nostr.Event, error) {
 	log.Println("QueryEvent", filter.Kinds)
 
-	ch := make(chan *nostr.Event, len(d.relays))
+	ch := make(chan *nostr.Event)
+	sub := d.pool.SubMany(context.Background(), d.relays, nostr.Filters{filter})
 	go func() {
 		defer close(ch)
-
-		var wg sync.WaitGroup
-		var mu sync.Mutex
-
-		m := make(map[string]*nostr.Event)
-		for _, r := range d.relays {
-			if r == nil {
-				continue
+		for {
+			v, ok := <-sub
+			if !ok {
+				break
 			}
-			wg.Add(1)
-			r := r
-			go func() {
-				defer wg.Done()
-				log.Printf("send query to %v", r.URL)
-				evs, err := r.QuerySync(context.Background(), *filter)
-				if err != nil {
-					log.Println("error", r.URL, err)
-					return
-				}
-				log.Printf("received result from %v", r.URL)
-				for _, ev := range evs {
-					mu.Lock()
-					m[ev.ID] = ev
-					mu.Unlock()
-				}
-			}()
+			ch <- v.Event
 		}
-		wg.Wait()
-
-		ids := []string{}
-		for k := range m {
-			ids = append(ids, k)
-		}
-		sort.Slice(ids, func(i, j int) bool {
-			return m[ids[i]].CreatedAt < m[ids[j]].CreatedAt
-		})
-		for _, id := range ids {
-			if rewriteURLs {
-				matches := urlRe.FindAllStringSubmatchIndex(m[id].Content, -1)
-				content := m[id].Content
-				result := ""
-				for _, m := range matches {
-					result += content[:m[0]]
-					result += "http://0.0.0.0:7447?url=" + url.QueryEscape(content[m[0]:m[1]])
-				}
-				if len(matches) > 0 {
-					result += content[matches[len(matches)-1][1]:]
-				}
-				m[id].Content = result
-			}
-			ch <- m[id]
-		}
-		log.Printf("received %d events", len(ids))
 	}()
 	return ch, nil
 }
 
 func main() {
-	flag.BoolVar(&rewriteURLs, "rewrite", false, "rewrite URLs")
 	flag.Parse()
 
-	var r forwarder
+	r := forwarder{
+		relays: []string{
+			"wss://nostr.compile-error.net/",
+			"wss://relay.nostr.band/",
+			"wss://nostr-relay.nokotaro.com/",
+			"wss://relay-jp.nostr.wirednet.jp/",
+			"wss://nostr.wine/",
+			"wss://yabu.me/",
+		},
+	}
 	server, err := relayer.NewServer(&r)
 	if err != nil {
 		log.Fatalf("failed to create server: %v", err)
 	}
-	server.Router().HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		u := r.URL.Query().Get("url")
-		resp, err := http.Get(u)
-		if err != nil {
-			log.Println(err)
-			return
-		}
-		defer resp.Body.Close()
-
-		if !strings.HasPrefix(resp.Header.Get("content-type"), "image/") {
-			http.Redirect(w, r, u, http.StatusFound)
-			return
-		}
-
-		for k, v := range resp.Header {
-			for _, vv := range v {
-				w.Header().Add(k, vv)
-			}
-		}
-		w.WriteHeader(resp.StatusCode)
-		io.Copy(w, resp.Body)
-	})
 	if err := server.Start("0.0.0.0", 7447); err != nil {
 		log.Fatalf("server terminated: %v", err)
 	}
